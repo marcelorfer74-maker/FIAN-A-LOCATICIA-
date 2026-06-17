9f70531e3c3a6317b0cee04da85d102cd22a65ee
marcelorfer74-maker <marcelorfer74@gmail.com>
2026-06-17 15:33:45 -0300

    Update service worker cache name and logic
    Updated cache name to 'fianca-pro-v6' and removed redundant cache activation logic.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fianca-pro-v5';
+const CACHE_NAME = 'fianca-pro-v6';
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -7,15 +7,6 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
